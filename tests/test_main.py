ff039fa362584237de78ef68f58b5ab2ed43d5ca
Hulage <rushikeshvasant.hulage@team.telstra.com>
2025-07-01 20:05:38 +0530

    Setup GitHub Actions Workflow

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import pytest
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "FastAPI is working without any extension!"}
